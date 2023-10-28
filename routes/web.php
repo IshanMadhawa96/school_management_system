7cb730c07484ab49a8a49c4985049d6542ea7e1d
ishanmadhawa <ishan@ninodigitalhouse.com>
2023-10-28 23:21:58 +0530

    admin login finished

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[AuthController::class,'login']);
+Route::get('logout',[AuthController::class,'logout']);
+Route::post('login',[AuthController::class,'AuthLogin']);
 
 Route::get('admin/dashboard', function () {
     return view('admin.dashboard');
